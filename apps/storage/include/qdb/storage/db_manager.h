3c4f2ad9835079f678e6554e62246727b778fe96
kirill-9887 <>
2026-04-29 16:33:42 +0000

    feat(storage): [STORAGE-2] implement db manager

---
+++ apps/storage/include/qdb/storage/db_manager.h
@@ -0,0 +1,48 @@
+#ifndef QUASARDB_DB_MANAGER_H
+#define QUASARDB_DB_MANAGER_H
+
+#include <string>
+#include <unordered_map>
+#include <filesystem>
+#include "database.h"
+
+namespace fs = std::filesystem;
+
+class DatabaseManager final {
+    fs::path _root;
+    std::unordered_map<std::string, Database> _databases;
+
+public:
+    DatabaseManager(std::string root) : _root(std::move(root)) {}
+
+    DatabaseManager(const DatabaseManager& other) = delete;
+    DatabaseManager& operator=(const DatabaseManager& other) = delete;
+    DatabaseManager(DatabaseManager&& other) noexcept = delete;
+    DatabaseManager& operator=(DatabaseManager&& other) noexcept = delete;
+
+    void create_database(const std::string& name) {
+        if (_databases.find(name) != _databases.end()) {
+            throw std::runtime_error("DB with name " + name + " already exists.");
+        }
+        fs::path db_path = fs::path(_root) / name;
+        fs::create_directory(db_path);
+        _databases.emplace(name, Database(name, db_path.string()));
+    }
+
+    void drop_database(const std::string& name) {
+        auto it = _databases.find(name);
+        if (it != _databases.end()) {
+            return;
+        }
+        fs::path db_path = fs::path(_root) / name;
+        fs::remove_all(db_path);
+        _databases.erase(it);
+    }
+
+    Database* use_database(const std::string& name){
+        auto it = _databases.find(name);
+        return (it != _databases.end()) ? &it->second : nullptr;
+    }
+};
+
+#endif  // QUASARDB_DB_H
